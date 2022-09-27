f17741bfebfefce2e6762c8fbe9b0fec9cecbff4
Pedro Luz <pedromsluz@gmail.com>
2022-09-27 18:51:03 +0100

    using clap to have some cli commands

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
  # mason_cli
 
 Mason CLI allows developers to create and use reusable templates.
+
+```bash
+mason new <block_name>
+```
+
+This will create a `<block_name>` folder at the current location.
+
+Inside the `blocks` directory, the structure will be like this:
+
+```bash
+> tree
+.
+└── hello
+    ├── __block__
+    │   └── README.md
+    └── mason.yml
+
+2 directories, 2 files    
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-#![allow(unused)]
+use clap::{arg, App, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = App::new("mason")
+        .bin_name("mason")
+        .about("Mason CLI")
+        .author("Pedro Luz <pedromsluz@gmail.com>")
+        .version("0.1.0")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("new")
+                .about("Create a new block")
+                .arg_required_else_help(true)
+                .arg(arg!(<block_name> "The name of the block")),
+        );
+    let matches = cmd.get_matches();
+
+    println!("{:#?}", matches);
 }
